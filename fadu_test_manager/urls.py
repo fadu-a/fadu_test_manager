07ccc9712e4a1b8f13602d5cff79ca06ebe1ce4b
Daeyoung Hong <dyhong@fadutec.com>
2018-04-19 18:05:15 +0900

    style($test_runners): fix hound ci violations

--- fadu_test_manager/urls.py
+++ fadu_test_manager/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
     path('runners/', include(('apps.urls', 'runners'), namespace="runners"))
 ]

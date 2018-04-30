52bdd1a7ffc0ab00c2d7b8d96022ed0a51df593b
jins <dlwlstlrsla@naver.com>
2018-04-30 15:23:51 +0900

    result 관련 내용 testcase/index 페이지에 링크, 중복된 view함수 제거

--- apps/urls.py
+++ apps/urls.py
@@ -5,9 +5,9 @@ from apps import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('<int:pk>', views.post_results, name='index2'),
-    path('results/', views.result_list, name='index3'),
-    path('results/<int:pk>', views.view_detail, name='result-detail'),
+    path('testcase/<int:pk>', views.post_results, name='index2'),
+    path('testcase/results/<int:pk>', views.result_list, name='index3'),
+    path('testcase/results/detail/<int:pk>', views.view_detail, name='result-detail'),
     url('^$', views.runner_check, name='check'),
     path('testcase/index/', views.get_index, name='index'),
     path('testcase/new/', views.post_new, name='new'),
--- apps/views.py
+++ apps/views.py
@@ -14,36 +14,11 @@ def post_results(request, pk):
     data["test_cases_id"] = pk
     test = TestIoLogs.objects.create(**data)
     test.save()
+    print("test")
     return HttpResponse(request.POST)
 
 
-def result_list(request):
-    servers = list(TestCases.objects.values('Title', 'id'))
-    print(servers)
-    servers2 = {}
-    servers2['datas'] = servers
-
-    return render(request, 'results/result_form.html', servers2)
-
-
-def view_detail(request, pk):
-    data = TestIoLogs.objects.filter(test_cases_id=pk)
-    data2 = {}
-    data2['datas'] = data
-    print(data2)
-    return render(request, 'results/result_detail.html', data2)
-
-
-@csrf_exempt
-def post_results(request, pk):
-    data = json.loads(request.body)
-    data["test_cases_id"] = pk
-    test = TestIoLogs.objects.create(**data)
-    test.save()
-    return HttpResponse(request.POST)
-
-
-def result_list(request):
+def result_list(request, pk):
     servers = list(TestCases.objects.values('Title', 'id'))
     print(servers)
     servers2 = {}
